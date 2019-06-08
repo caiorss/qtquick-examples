2f78ff53d62d2ce3926bbb5c2c9ea264694ac108
CaioRS <caiorss.rodrigues@gmail.com>
2019-06-08 15:57:00 -0300

    Register types TodoModel and TodoList in QML backend.

--- main.cpp
+++ main.cpp
@@ -2,30 +2,31 @@
 #include <QQmlApplicationEngine>
 #include <QtWidgets/QFileSystemModel>
 #include <QtQuick/QtQuick>
+#include <QQmlContext>
+
+#include "todolist.hpp"
+#include "todomodel.hpp"
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
-#if 0
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-                         if (!obj && url == objUrl)
-                             QCoreApplication::exit(-1);
-                     }, Qt::QueuedConnection);
-    engine.load(url);
-#endif
-
-//    QFileSystemModel* model = new QFileSystemModel();
-//    model->setRootPath(QDir::homePath());
-//    model->resolveSymlinks();
+    qmlRegisterType<TodoModel>("ToDo", 1, 0, "TodoModel");
+    qmlRegisterUncreatableType<TodoList>("ToDo", 1, 0, "ToDoList"
+                              ,"ToDoList should not be created in QML");
 
     QQuickView* view = new QQuickView();
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     view->setSource(url);
     view->show();
+
+    TodoList toDoList;
+
+    TodoModel* model = new TodoModel(view);
+    view->rootContext()->setContextProperty("toDoList", &toDoList);
+    // view->rootContext()->setContextProperty("TodoModel", model);
 
     //QQmlApplicationEngine engine;
     // view->rootContext()->setContextProperty("fileSystemModel", model);
