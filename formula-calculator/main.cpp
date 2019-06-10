ef09039c8bd03cf98f0f238f030840f6398126f8
CaioRS <caiorss.rodrigues@gmail.com>
2019-06-10 00:48:56 -0300

    Register type BlsCalculator on QmlEngine.

--- formula-calculator/main.cpp
+++ formula-calculator/main.cpp
@@ -3,14 +3,21 @@
 #include <QtQuick/QtQuick>
 #include <QQmlContext>
 
+#include "blscalculator.hpp"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
 
+    qmlRegisterType<BlsCalculator>("com.mycompany.blscalculator", 1, 0, "BlsCalculator");
+
     QQmlApplicationEngine engine;
     engine.load("qrc:/main.qml");
+
+    // BlsCalculator calc;
+    //engine.rootContext()->setContextProperty("blsCalculator", &calc);
+
     if(engine.rootObjects().isEmpty())
         return EXIT_FAILURE;
 
